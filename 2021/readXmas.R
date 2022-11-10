4b3a90abe92898b9f946d2b7f9827647bd60cc43
NNaikp <aikp@novonordisk.com>
2022-11-10 15:12:13 +0100

    Try it 2021 puzzles - starting with 01

---
+++ 2021/readXmas.R
@@ -0,0 +1,11 @@
+
+# Function to read data from AOC
+# Copied from https://colin-fraser.net/post/a-quick-tutorial-on-importing-data-from-advent-of-code-into-r/
+
+
+
+readXmas <- function(date) {
+  
+  
+  
+}
